98fb99f7115137248f01469261da5e6a3203a8fe
zhangyong.86 <zhangyong.86@bytedance.com>
2023-05-26 17:44:22 +0800

    fix: 添加@babel/runtime

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const About = () => {
-  return <>About</>;
+  const [inputValue, setInputValue] = React.useState('');
+  const [searchQuery, setSearchQuery] = React.useState<string[]>([]);
+  const [isPending, startTransition] = React.useTransition();
+  
+  const handleChange = (e: any) => {
+    setInputValue(e.target.value);
+    startTransition(() => setSearchQuery(Array(20000).fill(e.target.value)));
+    
+  };
+  
+  return (
+    <div>
+      <input onChange={handleChange} />
+      {isPending ? <p>loading....</p> : searchQuery.map((item, index) => (<p key={index}>{item}</p>))}
+    </div>
+  );
 };
 
 export default About;
